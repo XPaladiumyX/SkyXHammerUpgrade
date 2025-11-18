22327b851fb148440dead620939020e0f8007768
XPaladiumyX <allegra.yann@gmail.com>
2025-11-18 23:25:36 +0100

    first commit

---
+++ src/main/java/skyxnetwork/skyXHammerUpgrade/SkyXHammerUpgrade.java
@@ -0,0 +1,17 @@
+package skyxnetwork.skyXHammerUpgrade;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class SkyXHammerUpgrade extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
